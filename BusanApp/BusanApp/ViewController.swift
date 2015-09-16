0f7be509607f599707023abd722b27768dde659f
chansung <chansung18@gmail.com>
2015-09-16 11:10:54 +0900

    initial project creation.

---
+++ BusanApp/BusanApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusanApp
+//
+//  Created by chansung on 9/16/15.
+//  Copyright (c) 2015 Eunkyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

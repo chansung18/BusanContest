813922778fa57db25866c7b427c251fe66efa203
chansung <chansung18@gmail.com>
2015-09-18 21:12:53 +0900

    fixed bug.

--- BusanApp/BusanApp/DustRaito.swift
+++ BusanApp/BusanApp/DustRaito.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class DustRaito: UIViewController, NSXMLParserDelegate{
+class DustRaito: UIViewController, NSXMLParserDelegate {
     
     
 
@@ -23,34 +23,37 @@ class DustRaito: UIViewController, NSXMLParserDelegate{
     
     override func viewDidLoad() {
         
+        print("????")
+        
         beginParsing()
         
     }
     
     func beginParsing()
     {
-       var baseUrl = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList?tmX=244148.546388&tmY=412423.75772&pageNo=1&numOfRows=10&ServiceKey="
-       var serviceKey = "AOzAtUciruP7pIKwP7KO0%2BGWMJV7VWcqhsy1vM0VBuqveoL0sY4GC7s8HGVUXblNGVhsK23WPlqHaWEc8EVrbA%3D%3D"
-       var endcodedName = serviceKey.stringByReplacingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+        //http://kosis.kr/kosisapi/service/IndicatorService/IndListSearchRequest?STAT_JIPYO_NM=%EA%B0%95%EC%88%98%EB%9F%89&ServiceKey=
+        let baseUrl = "http://kosis.kr/kosisapi/service/IndicatorService/IndListSearchRequest?STAT_JIPYO_NM=%EA%B0%95%EC%88%98%EB%9F%89&ServiceKey="
+        let serviceKey = "AOzAtUciruP7pIKwP7KO0%2BGWMJV7VWcqhsy1vM0VBuqveoL0sY4GC7s8HGVUXblNGVhsK23WPlqHaWEc8EVrbA%3D%3D"
         
-        
-        
-        
-        
-        
-        
+        let url = baseUrl + serviceKey
         
         posts = []
-        parser = NSXMLParser(contentsOfURL:(NSURL(string:serviceKey )!))!
+        parser = NSXMLParser(contentsOfURL:(NSURL(string:url)!))!
         parser.delegate = self
         
         parser.parse()
-        
-        
-        
     }
     
+    func parserDidStartDocument(parser: NSXMLParser) {
+        print("hello world")
+    }
     
+    func parser(parser: NSXMLParser, validationErrorOccurred validationError: NSError) {
+        print("eeror")
+    }
     
+    func parser(parser: NSXMLParser, parseErrorOccurred parseError: NSError) {
+        print("errorrrrr")
+    }
 }
 

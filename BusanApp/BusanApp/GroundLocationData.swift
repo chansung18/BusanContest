96b04b0cc5f132fbba62423afb62fbaffdf5b8e5
Me <eunkyoseo@chansungs-mac-mini.local>
2015-09-28 17:53:29 +0900

    location

---
+++ BusanApp/BusanApp/GroundLocationData.swift
@@ -0,0 +1,36 @@
+//
+//  GroundLocationData.swift
+//  BusanApp
+//
+//  Created by Eunkyo, Seo on 9/28/15.
+//  Copyright © 2015 Eunkyo. All rights reserved.
+//
+
+import Foundation
+
+
+func ==(lhs: GroundLocationData, rhs: GroundLocationData) -> Bool{
+    
+    return lhs.hashValue == rhs.hashValue
+    
+}
+
+class GroundLocationData: Hashable{
+    var longitude: Double
+    var latitude: Double
+    
+    var hashValue: Int{
+        return "\(self.longitude),\(self.latitude)".hashValue
+    }
+    
+    init(longitude: Double, latitude: Double)
+    {
+        self.longitude = longitude
+        self.latitude = latitude
+    }
+    
+    
+
+    
+    
+}

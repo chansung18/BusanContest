355770c1575185616150609c889c18c2fec32c3b
chansung <chansung18@gmail.com>
2015-09-30 15:49:16 +0900

    asdfasdf

--- BusanApp/BusanApp/WaterViewController.swift
+++ BusanApp/BusanApp/WaterViewController.swift
@@ -11,14 +11,18 @@ import UIKit
 class WaterViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var provinceName: UILabel!
-  
     
     var waterDataSet: [WaterData] = [WaterData]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+        self.navigationController?.navigationBarHidden = false
+        self.navigationController?.navigationBar.topItem?.title = "식수질"
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,18 +43,18 @@ class WaterViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
     */
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.waterDataSet.count
+        return 10
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell : WaterViewCell = tableView.dequeueReusableCellWithIdentifier("WaterCell") as! WaterViewCell
-        print("dddd")
-        cell.streetName.text = waterDataSet[indexPath.row].inspecArea
-        cell.result.text = waterDataSet[indexPath.row].result
-        let chemicalElement = waterDataSet[indexPath.row].chemicalElements
-        cell.gernalGerm.text = "\(chemicalElement[5])"
-        cell.allGerm.text = "\(chemicalElement[6])"
-        cell .partGerm.text  =    "\(chemicalElement[7])"
+//        print("dddd")
+//        cell.streetName.text = waterDataSet[indexPath.row].inspecArea
+//        cell.result.text = waterDataSet[indexPath.row].result
+//        let chemicalElement = waterDataSet[indexPath.row].chemicalElements
+//        cell.gernalGerm.text = "\(chemicalElement[5])"
+//        cell.allGerm.text = "\(chemicalElement[6])"
+//        cell .partGerm.text  =    "\(chemicalElement[7])"
         
     
         return cell
